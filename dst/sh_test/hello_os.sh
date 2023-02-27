1dad59c4259ed65b15536f37c9264b60eaa847b9
柳政尧 <21371300@buaa.edu.cn>
2023-02-27 17:24:08 +0800

    add sh_test

---
+++ dst/sh_test/hello_os.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+input=$1
+output=$2
+i=0
+while read line; do
+	i=$(($i+1))
+	if (($i==8)); then
+		echo $line > $output
+	elif (($i==32)); then
+		echo $line >> $output
+	elif (($i==128)); then
+		echo $line >> $output
+	elif (($i==512)); then
+		echo $line >> $output
+	elif (($i==1024)); then
+		echo $line >> $output
+	fi
+done < $input
+
