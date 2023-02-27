a73c64995507c27d972c6f4ba0e62b0b411a35c3
柳政尧 <21371300@buaa.edu.cn>
2023-02-27 19:41:13 +0800

    add search.sh

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+
+input=$1
+str=$2
+output=$3
+i=1
+
+echo -n "" > $output	# create or clear file
+while read line; do
+	if [[ $line == *${str}* ]]; then
+		# echo ${line}
+		if (($i==1)); then
+			echo $i >> $output
+		else
+			echo $i >> $output
+		fi
+	fi
+	i=$(($i+1))
+done < $input
