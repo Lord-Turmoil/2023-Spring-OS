0b87eaf4531ef4dd61fdfc061dfd931d86baa498
柳政尧 <21371300@buaa.edu.cn>
2023-02-27 19:13:52 +0800

    remove line feed

--- dst/sh_test/hello_os.sh
+++ dst/sh_test/hello_os.sh
@@ -3,19 +3,19 @@
 input=$1
 output=$2
 i=0
-IFS=$'\n'
+# IFS=$'\n'
 while read line; do
 	i=$(($i+1))
 	if (($i==8)); then
-		echo $line > $output
+		echo -n $line > $output
 	elif (($i==32)); then
-		echo $line >> $output
+		echo -n $line >> $output
 	elif (($i==128)); then
-		echo $line >> $output
+		echo -n $line >> $output
 	elif (($i==512)); then
-		echo $line >> $output
+		echo -n $line >> $output
 	elif (($i==1024)); then
-		echo $line >> $output
+		echo -n $line >> $output
 		break;
 	fi
 done < $input
