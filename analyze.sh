56e26904821be7c6333a3a0107dcd5259d1ec090
柳政尧 <21371300@buaa.edu.cn>
2023-03-06 19:59:07 +0800

    first commit

--- analyze.sh
+++ analyze.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-if [ $# -eq 1 ];
-then
+if [ $# -eq 1 ]; then
     # Your code here. (1/4)
-
+	grep -E "ERROR|WARN" $1 >> bug.txt
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-
+		tail -5 $1
     ;;
     "--find")
         # Your code here. (3/4)
-
+		grep -E $3 $1 > "${3}.txt"
     ;;
     "--diff")
         # Your code here. (4/4)
-
+		if diff $1 $3 > /dev/null; then
+			echo same
+		else
+			echo different
+		fi		
     ;;
     esac
 fi
