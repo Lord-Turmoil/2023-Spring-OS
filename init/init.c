a0e4a330e2fbc7f6c8087c81245f2f514728e44d
柳政尧 <21371300@buaa.edu.cn>
2023-03-07 15:25:44 +0800

    feat: complete printk

--- init/init.c
+++ init/init.c
@@ -11,8 +11,30 @@
 #include <generated/init_override.h>
 #else
 
+#define _INTERNAL
+
 void mips_init() {
 	printk("init.c:\tmips_init() is called\n");
+
+#ifdef _INTERNAL
+	printk("\n");
+	printk("%%[flags][width][length]<specifier>: [%-10ld]\n", 75159);
+	printk("%%[flags][width][length]<specifier>: [%010ld]\n", -75159);
+	printk("%%[flags][width][------]<specifier>: [%-10x]\n", 66);
+	printk("%%[flags][-----][length]<specifier>: [%-lX]\n", 66);
+	printk("%%[-----][width][length]<specifier>: [%10lo]\n", 66);
+	printk("%%[flags][-----][------]<specifier>: [%10O]\n", 66);
+	printk("%%[-----][width][------]<specifier>: [%10u]\n", 66);
+	printk("%%[-----][-----][length]<specifier>: [%lU]\n", 66);
+	printk("%%[-----][-----][------]<specifier>: [%d]\n\n", -1957);
+	
+	printk("%%[flags][width][------]<--------->: [%-10]\n\n", 456);	// error
+
+	printk("%%[-----][width][------]<specifier>: [%20s]\n", "Test String");
+	printk("%%[-----][-----][------]<specifier>: [%s]\n\n", "Test String");
+
+	printk("%%[-----][-----][------]<specifier>: [%d]%s[%X]\n\n", 2015, " VS ", 1986);
+#endif
 
 	// lab2:
 	// mips_detect_memory();
@@ -42,6 +64,7 @@ void mips_init() {
 	// lab3:
 	// kclock_init();
 	// enable_irq();
+
 	while (1) {
 	}
 }
--- lib/print.c
+++ lib/print.c
@@ -1,13 +1,19 @@
 #include <print.h>
 
 /* forward declaration */
-static void print_char(fmt_callback_t, void *, char, int, int);
-static void print_str(fmt_callback_t, void *, const char *, int, int);
-static void print_num(fmt_callback_t, void *, unsigned long, int, int, int, int, char, int);
+static void print_char(fmt_callback_t, void*, char, int, int);
+static void print_str(fmt_callback_t, void*, const char*, int, int);
+static void print_num(fmt_callback_t, void*, unsigned long, int, int, int, int, char, int);
 
-void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
+static int is_digit(int c);
+static int is_alpha(int c);
+static int is_upper(int c);
+static int is_lower(int c);
+
+void vprintfmt(fmt_callback_t out, void* data, const char* fmt, va_list ap)
+{
 	char c;
-	const char *s;
+	const char* s;
 	long num;
 
 	int width;
@@ -16,34 +22,83 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 	int ladjust;   // output is left-aligned
 	char padc;     // padding char
 
-	for (;;) {
+	
+
+	for (;;)
+	{
 		/* scan for the next '%' */
 		/* Exercise 1.4: Your code here. (1/8) */
+		const char* fallback = fmt;
+		while (*fmt && (*fmt != '%'))
+			++fmt;
 
 		/* flush the string found so far */
 		/* Exercise 1.4: Your code here. (2/8) */
+		out(data, fallback, fmt - fallback);	// will be invoked even if _ is 0
 
 		/* check "are we hitting the end?" */
 		/* Exercise 1.4: Your code here. (3/8) */
+		if (!*fmt)
+			return;
 
+		/*
+		* Notice that, printf will discard bad format specifier.
+		* e.g. printf("[%-12]") will output only "[]".
+		*/
+
+		/* %[flags][width][length]<specifier> */
 		/* we found a '%' */
 		/* Exercise 1.4: Your code here. (4/8) */
+		++fmt;
 
 		/* check format flag */
 		/* Exercise 1.4: Your code here. (5/8) */
+		padc = ' ';
+		ladjust = 0;
+		if (*fmt == '-')
+		{
+			ladjust = 1;
+			++fmt;
+		}
+		else if (*fmt == '0')
+		{
+			padc = '0';
+			++fmt;
+		}
+		// *fmt could be '\0' here.
 
 		/* get width */
 		/* Exercise 1.4: Your code here. (6/8) */
+		width = 0;
+		while (*fmt && is_digit(*fmt))	// only accept decimal
+		{
+			width = width * 10 + (*fmt - '0');
+			++fmt;
+		}
+		// *fmt could be '\0' here.
 
 		/* check for long */
 		/* Exercise 1.4: Your code here. (7/8) */
+		if (*fmt == 'l')
+		{
+			long_flag = 1;
+			++fmt;
+		}
+		else
+			long_flag = 0;
 
-		neg_flag = 0;
-		switch (*fmt) {
+		// *fmt could be '\0' here.
+		
+		neg_flag = 0; // unnecessary
+		switch (*fmt)
+		{
 		case 'b':
-			if (long_flag) {
+			if (long_flag)
+			{
 				num = va_arg(ap, long int);
-			} else {
+			}
+			else
+			{
 				num = va_arg(ap, int);
 			}
 			print_num(out, data, num, 2, 0, width, ladjust, padc, 0);
@@ -51,9 +106,12 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 
 		case 'd':
 		case 'D':
-			if (long_flag) {
+			if (long_flag)
+			{
 				num = va_arg(ap, long int);
-			} else {
+			}
+			else
+			{
 				num = va_arg(ap, int);
 			}
 
@@ -62,15 +120,23 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			 * complete this part. Think the differences between case 'd' and the
 			 * others. (hint: 'neg_flag').
 			 */
-			/* Exercise 1.4: Your code here. (8/8) */
-
+			 /* Exercise 1.4: Your code here. (8/8) */
+			if (num < 0)	// print_num only accept positive number.
+			{
+				neg_flag = 1;
+				num = -num;
+			}
+			print_num(out, data, num, 10, neg_flag, width, ladjust, padc, 0);
 			break;
 
 		case 'o':
 		case 'O':
-			if (long_flag) {
+			if (long_flag)
+			{
 				num = va_arg(ap, long int);
-			} else {
+			}
+			else
+			{
 				num = va_arg(ap, int);
 			}
 			print_num(out, data, num, 8, 0, width, ladjust, padc, 0);
@@ -78,27 +144,36 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 
 		case 'u':
 		case 'U':
-			if (long_flag) {
+			if (long_flag)
+			{
 				num = va_arg(ap, long int);
-			} else {
+			}
+			else
+			{
 				num = va_arg(ap, int);
 			}
 			print_num(out, data, num, 10, 0, width, ladjust, padc, 0);
 			break;
 
 		case 'x':
-			if (long_flag) {
+			if (long_flag)
+			{
 				num = va_arg(ap, long int);
-			} else {
+			}
+			else
+			{
 				num = va_arg(ap, int);
 			}
 			print_num(out, data, num, 16, 0, width, ladjust, padc, 0);
 			break;
 
 		case 'X':
-			if (long_flag) {
+			if (long_flag)
+			{
 				num = va_arg(ap, long int);
-			} else {
+			}
+			else
+			{
 				num = va_arg(ap, int);
 			}
 			print_num(out, data, num, 16, 0, width, ladjust, padc, 1);
@@ -110,7 +185,7 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			break;
 
 		case 's':
-			s = (char *)va_arg(ap, char *);
+			s = (char*)va_arg(ap, char*);
 			print_str(out, data, s, width, ladjust);
 			break;
 
@@ -122,57 +197,74 @@ void vprintfmt(fmt_callback_t out, void *data, const char *fmt, va_list ap) {
 			/* output this char as it is */
 			out(data, fmt, 1);
 		}
+
 		fmt++;
-	}
+	}	// for (;;)
 }
 
 /* --------------- local help functions --------------------- */
-void print_char(fmt_callback_t out, void *data, char c, int length, int ladjust) {
+void print_char(fmt_callback_t out, void* data, char c, int length, int ladjust)
+{
 	int i;
 
-	if (length < 1) {
+	if (length < 1)
+	{
 		length = 1;
 	}
 	const char space = ' ';
-	if (ladjust) {
+	if (ladjust)
+	{
 		out(data, &c, 1);
-		for (i = 1; i < length; i++) {
+		for (i = 1; i < length; i++)
+		{
 			out(data, &space, 1);
 		}
-	} else {
-		for (i = 0; i < length - 1; i++) {
+	}
+	else
+	{
+		for (i = 0; i < length - 1; i++)
+		{
 			out(data, &space, 1);
 		}
 		out(data, &c, 1);
 	}
 }
 
-void print_str(fmt_callback_t out, void *data, const char *s, int length, int ladjust) {
+void print_str(fmt_callback_t out, void* data, const char* s, int length, int ladjust)
+{
 	int i;
 	int len = 0;
-	const char *s1 = s;
-	while (*s1++) {
+	const char* s1 = s;
+	while (*s1++)
+	{
 		len++;
 	}
-	if (length < len) {
+	if (length < len)
+	{
 		length = len;
 	}
 
-	if (ladjust) {
+	if (ladjust)
+	{
 		out(data, s, len);
-		for (i = len; i < length; i++) {
+		for (i = len; i < length; i++)
+		{
 			out(data, " ", 1);
 		}
-	} else {
-		for (i = 0; i < length - len; i++) {
+	}
+	else
+	{
+		for (i = 0; i < length - len; i++)
+		{
 			out(data, " ", 1);
 		}
 		out(data, s, len);
 	}
 }
 
-void print_num(fmt_callback_t out, void *data, unsigned long u, int base, int neg_flag, int length,
-	       int ladjust, char padc, int upcase) {
+void print_num(fmt_callback_t out, void* data, unsigned long u, int base, int neg_flag, int length,
+			   int ladjust, char padc, int upcase)
+{
 	/* algorithm :
 	 *  1. prints the number from left to right in reverse form.
 	 *  2. fill the remaining spaces with padc if length is longer than
@@ -185,42 +277,56 @@ void print_num(fmt_callback_t out, void *data, unsigned long u, int base, int ne
 
 	int actualLength = 0;
 	char buf[length + 70];
-	char *p = buf;
+	char* p = buf;
 	int i;
 
-	do {
+	do
+	{
 		int tmp = u % base;
-		if (tmp <= 9) {
+		if (tmp <= 9)
+		{
 			*p++ = '0' + tmp;
-		} else if (upcase) {
+		}
+		else if (upcase)
+		{
 			*p++ = 'A' + tmp - 10;
-		} else {
+		}
+		else
+		{
 			*p++ = 'a' + tmp - 10;
 		}
 		u /= base;
 	} while (u != 0);
 
-	if (neg_flag) {
+	if (neg_flag)
+	{
 		*p++ = '-';
 	}
 
 	/* figure out actual length and adjust the maximum length */
 	actualLength = p - buf;
-	if (length < actualLength) {
+	if (length < actualLength)
+	{
 		length = actualLength;
 	}
 
 	/* add padding */
-	if (ladjust) {
+	if (ladjust)
+	{
 		padc = ' ';
 	}
-	if (neg_flag && !ladjust && (padc == '0')) {
-		for (i = actualLength - 1; i < length - 1; i++) {
+	if (neg_flag && !ladjust && (padc == '0'))
+	{
+		for (i = actualLength - 1; i < length - 1; i++)
+		{
 			buf[i] = padc;
 		}
 		buf[length - 1] = '-';
-	} else {
-		for (i = actualLength; i < length; i++) {
+	}
+	else
+	{
+		for (i = actualLength; i < length; i++)
+		{
 			buf[i] = padc;
 		}
 	}
@@ -228,14 +334,18 @@ void print_num(fmt_callback_t out, void *data, unsigned long u, int base, int ne
 	/* prepare to reverse the string */
 	int begin = 0;
 	int end;
-	if (ladjust) {
+	if (ladjust)
+	{
 		end = actualLength - 1;
-	} else {
+	}
+	else
+	{
 		end = length - 1;
 	}
 
 	/* adjust the string pointer */
-	while (end > begin) {
+	while (end > begin)
+	{
 		char tmp = buf[begin];
 		buf[begin] = buf[end];
 		buf[end] = tmp;
@@ -244,4 +354,24 @@ void print_num(fmt_callback_t out, void *data, unsigned long u, int base, int ne
 	}
 
 	out(data, buf, length);
+}
+
+static int is_digit(int c)
+{
+	return ('0' <= c) && (c <= '9');
+}
+
+static int is_alpha(int c)
+{
+	return is_upper(c) || is_lower(c);
+}
+
+static int is_upper(int c)
+{
+	return ('A' <= c) && (c <= 'Z');
+}
+
+static int is_lower(int c)
+{
+	return ('a' <= c) && (c <= 'Z');
 }
