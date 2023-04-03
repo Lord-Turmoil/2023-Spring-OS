d623f1f942e78d5ea470072943596b4d5ef44ab8
柳政尧 <21371300@buaa.edu.cn>
2023-04-03 17:45:31 +0800

    feat: complete env_init

--- kern/env.c
+++ kern/env.c
@@ -163,13 +163,19 @@ void env_init(void)
 	/* Step 1: Initialize 'env_free_list' with 'LIST_INIT' and 'env_sched_list'
 	 * with 'TAILQ_INIT'. */
 	/* Exercise 3.1: Your code here. (1/2) */
+	LIST_INIT(&env_free_list);
+	TAILQ_INIT(&env_sched_list);
 
 	/* Step 2: Traverse the elements of 'envs' array, set their status to
 	 * 'ENV_FREE' and insert them into the 'env_free_list'. Make sure, after
 	 * the insertion, the order of envs in the list should be the same as they
 	 * are in the 'envs' array. */
-
 	/* Exercise 3.1: Your code here. (2/2) */
+	for (int i = NENV - 1; i >= 0; i--)
+	{
+		envs[i].env_status = ENV_FREE;
+		LIST_INSERT_HEAD(&env_free_list, &envs[i], env_link);
+	}
 
 	/*
 	 * We want to map 'UPAGES' and 'UENVS' to *every* user space with PTE_G
