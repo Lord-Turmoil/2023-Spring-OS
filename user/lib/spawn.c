3e7d5240c7cfc5918659295b20b92104d45577c3
柳政尧 <21371300@buaa.edu.cn>
2023-05-20 16:05:27 +0800

    feat: complete spawn

--- user/lib/spawn.c
+++ user/lib/spawn.c
@@ -5,38 +5,44 @@
 
 #define debug 0
 
-int init_stack(u_int child, char **argv, u_int *init_sp) {
+int init_stack(u_int child, char** argv, u_int* init_sp)
+{
 	int argc, i, r, tot;
-	char *strings;
-	u_int *args;
+	char* strings;
+	u_int* args;
 
 	// Count the number of arguments (argc)
 	// and the total amount of space needed for strings (tot)
 	tot = 0;
-	for (argc = 0; argv[argc]; argc++) {
+	for (argc = 0; argv[argc]; argc++)
+	{
 		tot += strlen(argv[argc]) + 1;
 	}
 
 	// Make sure everything will fit in the initial stack page
-	if (ROUND(tot, 4) + 4 * (argc + 3) > BY2PG) {
+	if (ROUND(tot, 4) + 4 * (argc + 3) > BY2PG)
+	{
 		return -E_NO_MEM;
 	}
 
 	// Determine where to place the strings and the args array
-	strings = (char *)(UTEMP + BY2PG) - tot;
-	args = (u_int *)(UTEMP + BY2PG - ROUND(tot, 4) - 4 * (argc + 1));
+	strings = (char*)(UTEMP + BY2PG) - tot;
+	args = (u_int*)(UTEMP + BY2PG - ROUND(tot, 4) - 4 * (argc + 1));
 
-	if ((r = syscall_mem_alloc(0, (void *)UTEMP, PTE_D)) < 0) {
+	if ((r = syscall_mem_alloc(0, (void*)UTEMP, PTE_D)) < 0)
+	{
 		return r;
 	}
 
 	// Copy the argument strings into the stack page at 'strings'
-	char *ctemp, *argv_temp;
+	char* ctemp, * argv_temp;
 	u_int j;
 	ctemp = strings;
-	for (i = 0; i < argc; i++) {
+	for (i = 0; i < argc; i++)
+	{
 		argv_temp = argv[i];
-		for (j = 0; j < strlen(argv[i]); j++) {
+		for (j = 0; j < strlen(argv[i]); j++)
+		{
 			*ctemp = *argv_temp;
 			ctemp++;
 			argv_temp++;
@@ -45,11 +51,12 @@ int init_stack(u_int child, char **argv, u_int *init_sp) {
 		ctemp++;
 	}
 
-	// Initialize args[0..argc-1] to be pointers to these strings
+	// Initialize args[0 ... argc - 1] to be pointers to these strings
 	// that will be valid addresses for the child environment
 	// (for whom this page will be at USTACKTOP-BY2PG!).
-	ctemp = (char *)(USTACKTOP - UTEMP - BY2PG + (u_int)strings);
-	for (i = 0; i < argc; i++) {
+	ctemp = (char*)(USTACKTOP - UTEMP - BY2PG + (u_int)strings);
+	for (i = 0; i < argc; i++)
+	{
 		args[i] = (u_int)ctemp;
 		ctemp += strlen(argv[i]) + 1;
 	}
@@ -61,7 +68,7 @@ int init_stack(u_int child, char **argv, u_int *init_sp) {
 	// Push two more words onto the child's stack below 'args',
 	// containing the argc and argv parameters to be passed
 	// to the child's main() function.
-	u_int *pargv_ptr;
+	u_int* pargv_ptr;
 	pargv_ptr = args - 1;
 	*pargv_ptr = USTACKTOP - UTEMP - BY2PG + (u_int)args;
 	pargv_ptr--;
@@ -70,42 +77,48 @@ int init_stack(u_int child, char **argv, u_int *init_sp) {
 	// Set *init_sp to the initial stack pointer for the child
 	*init_sp = USTACKTOP - UTEMP - BY2PG + (u_int)pargv_ptr;
 
-	if ((r = syscall_mem_map(0, (void *)UTEMP, child, (void *)(USTACKTOP - BY2PG), PTE_D)) <
-	    0) {
+	if ((r = syscall_mem_map(0, (void*)UTEMP, child, (void*)(USTACKTOP - BY2PG), PTE_D)) < 0)
+	{
 		goto error;
 	}
-	if ((r = syscall_mem_unmap(0, (void *)UTEMP)) < 0) {
+	if ((r = syscall_mem_unmap(0, (void*)UTEMP)) < 0)
+	{
 		goto error;
 	}
 
 	return 0;
 
 error:
-	syscall_mem_unmap(0, (void *)UTEMP);
+	syscall_mem_unmap(0, (void*)UTEMP);
 	return r;
 }
 
-static int spawn_mapper(void *data, u_long va, size_t offset, u_int perm, const void *src,
-			size_t len) {
-	u_int child_id = *(u_int *)data;
-	try(syscall_mem_alloc(child_id, (void *)va, perm));
-	if (src != NULL) {
-		int r = syscall_mem_map(child_id, (void *)va, 0, (void *)UTEMP, perm | PTE_D);
-		if (r) {
-			syscall_mem_unmap(child_id, (void *)va);
+static int spawn_mapper(void* data, u_long va, size_t offset, u_int perm, const void* src,
+						size_t len)
+{
+	u_int child_id = *(u_int*)data;
+	try(syscall_mem_alloc(child_id, (void*)va, perm));
+	if (src != NULL)
+	{
+		int r = syscall_mem_map(child_id, (void*)va, 0, (void*)UTEMP, perm | PTE_D);
+		if (r)
+		{
+			syscall_mem_unmap(child_id, (void*)va);
 			return r;
 		}
-		memcpy((void *)(UTEMP + offset), src, len);
-		return syscall_mem_unmap(0, (void *)UTEMP);
+		memcpy((void*)(UTEMP + offset), src, len);
+		return syscall_mem_unmap(0, (void*)UTEMP);
 	}
 	return 0;
 }
 
-int spawn(char *prog, char **argv) {
+int spawn(char* prog, char** argv)
+{
 	// Step 1: Open the file 'prog' (the path of the program).
 	// Return the error if 'open' fails.
 	int fd;
-	if ((fd = open(prog, O_RDONLY)) < 0) {
+	if ((fd = open(prog, O_RDONLY)) < 0)
+	{
 		return fd;
 	}
 
@@ -116,9 +129,15 @@ int spawn(char *prog, char **argv) {
 	int r;
 	u_char elfbuf[512];
 	/* Exercise 6.4: Your code here. (1/6) */
+	if (readn(fd, elfbuf, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
+	{
+		r = -E_NOT_EXEC;
+		goto err;
+	}
 
-	const Elf32_Ehdr *ehdr = elf_from(elfbuf, sizeof(Elf32_Ehdr));
-	if (!ehdr) {
+	const Elf32_Ehdr* ehdr = elf_from(elfbuf, sizeof(Elf32_Ehdr));
+	if (!ehdr)
+	{
 		r = -E_NOT_EXEC;
 		goto err;
 	}
@@ -128,35 +147,52 @@ int spawn(char *prog, char **argv) {
 	// If the syscall fails, set 'r' and 'goto err'.
 	u_int child;
 	/* Exercise 6.4: Your code here. (2/6) */
+	r = syscall_exofork();
+	if (r < 0)
+		goto err;
+	child = r;
 
 	// Step 4: Use 'init_stack(child, argv, &sp)' to initialize the stack of the child.
 	// 'goto err1' if that fails.
 	u_int sp;
 	/* Exercise 6.4: Your code here. (3/6) */
+	init_stack(child, argv, &sp);
 
 	// Step 5: Load the ELF segments in the file into the child's memory.
 	// This is similar to 'load_icode()' in the kernel.
 	size_t ph_off;
-	ELF_FOREACH_PHDR_OFF (ph_off, ehdr) {
+	ELF_FOREACH_PHDR_OFF(ph_off, ehdr)
+	{
 		// Read the program header in the file with offset 'ph_off' and length
 		// 'ehdr->e_phentsize' into 'elfbuf'.
 		// 'goto err1' on failure.
 		// You may want to use 'seek' and 'readn'.
 		/* Exercise 6.4: Your code here. (4/6) */
+		seek(fd, ph_off);
+		r = readn(fd, elfbuf, ehdr->e_phentsize);
+		if (r != ehdr->e_phentsize)
+		{
+			r = -E_NOT_EXEC;
+			goto err1;
+		}
 
-		Elf32_Phdr *ph = (Elf32_Phdr *)elfbuf;
-		if (ph->p_type == PT_LOAD) {
-			void *bin;
+		Elf32_Phdr* ph = (Elf32_Phdr*)elfbuf;
+		if (ph->p_type == PT_LOAD)
+		{
+			void* bin;
 			// Read and map the ELF data in the file at 'ph->p_offset' into our memory
 			// using 'read_map()'.
 			// 'goto err1' if that fails.
 			/* Exercise 6.4: Your code here. (5/6) */
+			if ((r = read_map(fd, ph_off, &bin)) != 0)
+				goto err1;
 
 			// Load the segment 'ph' into the child's memory using 'elf_load_seg()'.
 			// Use 'spawn_mapper' as the callback, and '&child' as its data.
 			// 'goto err1' if that fails.
 			/* Exercise 6.4: Your code here. (6/6) */
-
+			if ((r = elf_load_seg(ph, bin, spawn_mapper, &child)) != 0)
+				goto err1;
 		}
 	}
 	close(fd);
@@ -164,22 +200,28 @@ int spawn(char *prog, char **argv) {
 	struct Trapframe tf = envs[ENVX(child)].env_tf;
 	tf.cp0_epc = entrypoint;
 	tf.regs[29] = sp;
-	if ((r = syscall_set_trapframe(child, &tf)) != 0) {
+	if ((r = syscall_set_trapframe(child, &tf)) != 0)
+	{
 		goto err2;
 	}
 
 	// Pages with 'PTE_LIBRARY' set are shared between the parent and the child.
-	for (u_int pdeno = 0; pdeno <= PDX(USTACKTOP); pdeno++) {
-		if (!(vpd[pdeno] & PTE_V)) {
+	for (u_int pdeno = 0; pdeno <= PDX(USTACKTOP); pdeno++)
+	{
+		if (!(vpd[pdeno] & PTE_V))
+		{
 			continue;
 		}
-		for (u_int pteno = 0; pteno <= PTX(~0); pteno++) {
+		for (u_int pteno = 0; pteno <= PTX(~0); pteno++)
+		{
 			u_int pn = (pdeno << 10) + pteno;
 			u_int perm = vpt[pn] & ((1 << PGSHIFT) - 1);
-			if ((perm & PTE_V) && (perm & PTE_LIBRARY)) {
-				void *va = (void *)(pn << PGSHIFT);
+			if ((perm & PTE_V) && (perm & PTE_LIBRARY))
+			{
+				void* va = (void*)(pn << PGSHIFT);
 
-				if ((r = syscall_mem_map(0, va, child, va, perm)) < 0) {
+				if ((r = syscall_mem_map(0, va, child, va, perm)) < 0)
+				{
 					debugf("spawn: syscall_mem_map %x %x: %d\n", va, child, r);
 					goto err2;
 				}
@@ -187,7 +229,8 @@ int spawn(char *prog, char **argv) {
 		}
 	}
 
-	if ((r = syscall_set_env_status(child, ENV_RUNNABLE)) < 0) {
+	if ((r = syscall_set_env_status(child, ENV_RUNNABLE)) < 0)
+	{
 		debugf("spawn: syscall_set_env_status %x: %d\n", child, r);
 		goto err2;
 	}
@@ -203,7 +246,8 @@ err:
 	return r;
 }
 
-int spawnl(char *prog, char *args, ...) {
+int spawnl(char* prog, char* args, ...)
+{
 	// Thanks to MIPS calling convention, the layout of arguments on the stack
 	// are straightforward.
 	return spawn(prog, &args);
