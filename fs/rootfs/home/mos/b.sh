c221106a7086e62561d6663a73609867cc6cdbfb
柳政尧 <21371300@buaa.edu.cn>
2023-06-17 23:33:31 +0800

    update: example

--- fs/rootfs/home/mos/b.sh
+++ fs/rootfs/home/mos/b.sh
@@ -2,10 +2,7 @@
 
 echo "Test B: Demonstration of ; and &"
 
-echo "/home/mos/timer 3000 aaa; /home/mos/timer 2000 bbb; /home/mos/timer 1000 aaa"
-/home/mos/timer 3000 aaa; /home/mos/timer 2000 bbb; /home/mos/timer 1000 aaa
-
-echo "/home/mos/timer 3000 aaa& /home/mos/timer 2000 bbb& /home/mos/timer 1000 aaa"
-/home/mos/timer 3000 aaa& /home/mos/timer 2000 bbb& /home/mos/timer 1000 aaa
+/home/mos/timer 3000 aaa; /home/mos/timer 2000 bbb; /home/mos/timer 1000 ccc
+/home/mos/timer 3000 aaa& /home/mos/timer 2000 bbb& /home/mos/timer 1000 ccc
 
 exit
